840f7c5a6cb864df0158966ed070b396465d627a
akima <akm2000@gmail.com>
2013-09-24 20:32:10 +0900

    レスポンスシグネチャ実装中・・・

--- spec/libgss/response_signatrue_spec.rb
+++ spec/libgss/response_signatrue_spec.rb
@@ -29,19 +29,62 @@ describe "response_signature" do
     end
   end
 
-  context "default" do
-    before{ @network = new_network.login! }
-    it_should_behave_like "action_request with response_signature"
+  describe "action_request" do
+    context "default" do
+      before{ @network = new_network.login! }
+      it_should_behave_like "action_request with response_signature"
+    end
+
+    context "1.0.0" do
+      before{ @network = new_network_with_options(api_version: "1.0.0").login! }
+      it_should_behave_like "action_request with response_signature"
+    end
+
+    context "1.1.0" do
+      before{ @network = new_network_with_options(api_version: "1.1.0").login! }
+      it_should_behave_like "action_request with response_signature"
+    end
   end
 
-  context "1.0.0" do
-    before{ @network = new_network_with_options(api_version: "1.0.0").login! }
-    it_should_behave_like "action_request with response_signature"
+
+  shared_examples_for "async_action_request with response_signature" do
+    context "valid" do
+      it do
+        req = @network.new_async_action_request
+        req.server_time
+        req.send_request
+      end
+    end
+
+    context "invalid response body" do
+      before do
+        HTTP::Message.any_instance.stub(:content){ {"outputs" => [{"result" => Time.now.to_i - 30.days, "id" => 1}]}.to_json }
+      end
+
+      it do
+        req = @network.new_async_action_request
+        req.server_time
+        expect{ req.send_request }.to raise_error(Libgss::ActionRequest::SignatureError)
+      end
+    end
   end
 
-  context "1.1.0" do
-    before{ @network = new_network_with_options(api_version: "1.1.0").login! }
-    it_should_behave_like "action_request with response_signature"
+  describe "async_action_request" do
+    context "default" do
+      before{ @network = new_network.login! }
+      it_should_behave_like "async_action_request with response_signature"
+    end
+
+    context "1.0.0" do
+      before{ @network = new_network_with_options(api_version: "1.0.0").login! }
+      it_should_behave_like "async_action_request with response_signature"
+    end
+
+    context "1.1.0" do
+      before{ @network = new_network_with_options(api_version: "1.1.0").login! }
+      it_should_behave_like "async_action_request with response_signature"
+    end
   end
+
 
 end
