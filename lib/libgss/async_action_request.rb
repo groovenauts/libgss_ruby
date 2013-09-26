d2fd77e9f4d83165c9317b3e089cac50b4b476e4
akima <akm2000@gmail.com>
2013-09-26 11:15:38 +0900

    不要なブロックを削除しました。また不要なインスタンス変数への保持も削除しました

--- lib/libgss/async_action_request.rb
+++ lib/libgss/async_action_request.rb
@@ -20,25 +20,22 @@ module Libgss
     end
 
     # アクション群を実行するために実際にHTTPリクエストを送信します。
-    def send_request(&callback)
+    def send_request
       res = network.httpclient_for_action.post(action_url, {"inputs" => @actions.map(&:to_hash)}.to_json, req_headers)
       r = process_response(res, :async_request)
       outputs = Outputs.new(r["outputs"])
-      # callback.call(@outputs) if callback
       @ids = outputs.map{|output| output['id'] }
       outputs
     end
 
-    def async_status(ids=nil, &callback)
+    def async_status(ids=nil)
       ids ||= @ids
       ids = [ids] unless ids.is_a?(::Array)
       raise Error, "failed to get response. please exec send_request before call." unless ids
 
       res = network.httpclient_for_action.post(result_url, {'input_ids' => ids}.to_json, req_headers)
       r = process_response(res, :aync_results)
-      @outputs = Outputs.new(r["outputs"])
-      callback.call(@outputs) if callback
-      @outputs
+      Outputs.new(r["outputs"])
     end
     alias :async_results :async_status
   end
